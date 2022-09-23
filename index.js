449062af6bb9a3f615dad0cdfced5c087a23c660
DmitryBalandin <balandinqwerty1983@gmail.com>
2022-09-23 15:47:41 +0500

    add parse docx to  array valume and modificate index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mammoth = require("mammoth");
 const fs = require('fs');
 let script = require('./script');
-let date;
+const { funcTableToArrayItem, funcArrayValueToNormDoubleArray, sum, title } = require("./modificatonDocument");
+
 
 //  fs.readFile("./example/example.txt", "utf-8", (err,result) => {
 //     console.log(result);
@@ -12,11 +13,20 @@ let date;
 mammoth.convertToHtml({ path: './documentdocx/order.docx' })
     .then((result) => {
         console.log(typeof result);
-        date = JSON.stringify(result);
-        console.log(typeof date);
-    });
-script = JSON.stringify(script);
-setTimeout(() => console.log("Hello"), 1000);
-setTimeout(() => fs.writeFile('./text.html', `${date}${script}`, () => { }), 2000);
+        
+        return JSON.stringify(result);
+    })
+    .then((result, reject) => {
+        fs.writeFile('./text2.html', `${result}`, () => { })
+        return (funcTableToArrayItem(result));
+    })
+    .then(result => funcArrayValueToNormDoubleArray(result))
+    .then(result =>console.log(result));
+    
 
-setTimeout(()=> fs.appendFile('./text.html',"ikoweioweffkindkondsfkon", () => { }),3000);
+
+// script = JSON.stringify(script);
+
+// setTimeout(() => fs.writeFile('./text.html', `${date}${script}`, () => { }), 2000);
+
+// setTimeout(()=> fs.appendFile('./text.html',"ikoweioweffkindkondsfkon", () => { }),3000);
---
+++ modificatonDocument.js
@@ -0,0 +1,62 @@
+
+const funcTableToArrayItem = function tableToArrayItem(valueString) {
+    let domTableString = JSON.stringify(valueString).match(/<table>.+?<\/table>/).join();
+    let arrayLineTable = Array.from(domTableString.match(/(<tr>.+?<\/tr>)/gi));
+
+    let arrayCellTable = arrayLineTable.map((value) => {
+        return value.toString().match(/(<td>.+?<\/td>)/gi)
+            .map(value => value.replace(/<.+?>/g, ""));
+
+    });
+    return arrayCellTable;
+}
+
+
+const funcArrayValueToNormDoubleArray = function arrayValueToNormDoubleArray(arrayValue) {
+    let mapValue;
+    let arrSort = [];
+    arrayValue.forEach((value, index) => {
+        if (sumLengthStringOnArr(value) < 20) {
+            mapValue = arrayValue.slice(index + 1);
+        }
+    })
+    let counterObj = counterLenghtRowSqyareArray(mapValue);
+    for (let key in counterObj) {
+        arrSort.push([key, counterObj[key]]);
+    }
+    arrSort.sort((a, b) => b[1] - a[1]);
+    return arrayFromPrimaryToFullNameAndStimulation(mapValue, arrSort);
+}
+
+
+function sumLengthStringOnArr(arr) {
+    return arr.reduce((sum, item) => sum + item.length, 0);
+}
+
+
+function counterLenghtRowSqyareArray(arr) {
+    let counteObj = {};
+    arr.forEach((item) => {
+        item.forEach((value, index) => {
+            !counteObj[index] ? counteObj[index] = value.length : counteObj[index] += value.length
+        })
+    })
+    return counteObj
+}
+
+
+function arrayFromPrimaryToFullNameAndStimulation(mapValue, arrSort) {
+    return mapValue.map((item) => {
+        let array = [];
+        item.forEach((value, index) => {
+            if (index == +arrSort[0][0] || index == +arrSort[1][0]) {
+                array.push(value);
+            }
+        })
+        return array;
+    })
+}
+
+
+
+module.exports = {funcTableToArrayItem, funcArrayValueToNormDoubleArray, };
