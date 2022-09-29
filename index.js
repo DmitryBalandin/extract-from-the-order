7b0cc0a6e0601fa075a968eb5d84c80973ec214e
DmitryBalandin <balandinqwerty1983@gmail.com>
2022-09-29 09:08:06 +0500

    add routing post, add sending file from server to client

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const mammoth = require("mammoth");
 const fs = require('fs');
-let script = require('./script');
 const docx = require("docx");
+const path = require('path');
+const express = require('express');
+const multer = require('multer');
+
+
+
+
+
 const { funcTableToArrayItem, funcArrayValueToNormDoubleArray } = require("./controllers/modificatonDocument");
 const createSectionExtract = require('./controllers/createDocExtract');
-
+let script = require('./script');
 
 const dateOrder = {
     date: '12.04.2022',
@@ -13,6 +20,32 @@ const dateOrder = {
     fullNameChief: "Тропин В.А.",
     rankChief: "капитан вн.сл.",
 };
+
+const app = express();
+const PORT = 3000;
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname));
+app.use(multer({dest:"uploads"}).single("filedata"));
+app.post("/upload", function (req, res, next) {
+   
+    let filedata = req.file;
+    console.log(filedata);
+    if(!filedata)
+        res.send("Ошибка при загрузке файла");
+    else
+    console.log(filedata.filename);
+        res.send("Файл загружен");
+});
+
+app.post('/',(req,res)=>{
+    const {date,numberDetachment,numberOrder,fullNameChief,fileOrder} = req.body;
+    console.log(req.path);
+    
+})
+
 
 
 log = () => {
@@ -43,6 +76,10 @@ log = () => {
 };
 
 
+app.get('/', (req, res) => {
+    res.sendFile(createPath('index'));
+  });
 
-
-log();
+app.listen(PORT,(error)=>{
+    error ? console.log(error) : console.log(`Listening port ${PORT}`);
+});
